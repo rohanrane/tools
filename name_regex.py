63dd8a83c8008fb3ab0427a16baf4f12238c023c
Rohan Rane <rohanr@vt.edu>
2023-02-16 11:36:17 -0500

    Create name_regex.py

---
+++ name_regex.py
@@ -0,0 +1,12 @@
+import re
+
+string = "John F. Kennedy and Jane O'Connor-Smith are attending the conference."
+
+# Define the regular expression pattern to match the first and last names
+pattern = r'\b[A-Z][a-z]+(?:\s[A-Z][a-z]+)?(?:\s[A-Z]\.)?(?:\s[A-Z][a-z]+(?:[-\'][A-Z][a-z]+)?)\b'
+
+# Use the findall() function from the re module to find all matches in the string
+matches = re.findall(pattern, string)
+
+# Print the matches
+print(matches)
